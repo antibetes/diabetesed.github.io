aec379bb9fe17a90f968d29d8a2676ca8e6de366
frostbird <stephenb04@icloud.com>
2017-02-27 13:12:17 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+document.body.innerHTML='<div class="nav"><a href="//diabetesed.github.io"><li>diabetesed</li></a> </div> \
+<div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//diabetesed.github.io/artery.css'
