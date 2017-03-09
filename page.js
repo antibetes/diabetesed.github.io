cf01cc79097f5bfee32e481b27be11ed0f15797f
frostbird <stephenb04@icloud.com>
2017-03-09 13:23:43 -0600

    Update page.js

--- page.js
+++ page.js
@@ -1,2 +1,2 @@
-document.body.innerHTML='<div class="nav"><a href="//diabetesed.github.io"><li>diabetesed</li></a> </div> \
+document.body.innerHTML='<div class="nav"><a href="//diabetesed.github.io/index.html"><li>diabetesed</li></a> </div> \
 <div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//diabetesed.github.io/artery.css'</div>
